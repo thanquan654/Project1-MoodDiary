70e30ccffa9947be37d900f8b17d2638479e2b99
LeDanhTien <danhtien2k4@gmail.com>
2025-10-01 16:53:23 +0700

    fix: lỗi đọc biến môi trường

--- app/backend/smart-diary/src/main/java/com/project1/smart_diary/SmartDiaryApplication.java
+++ app/backend/smart-diary/src/main/java/com/project1/smart_diary/SmartDiaryApplication.java
@@ -8,7 +8,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SmartDiaryApplication {
 
 	public static void main(String[] args) {
-
+		Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
+		dotenv.entries().forEach(entry ->
+				System.setProperty(entry.getKey(), entry.getValue())
+		);
 		SpringApplication.run(SmartDiaryApplication.class, args);
 	}
 
