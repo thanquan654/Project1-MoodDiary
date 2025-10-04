e58430280d27b66b4a3eabe87bcbec04d77c2d76
LeDanhTien <danhtien2k4@gmail.com>
2025-10-04 13:41:20 +0700

    feat: Thêm các abstract methods trong diaryRepo cho chức năng tìm kiếm nhật kí với date

--- app/backend/smart-diary/src/main/java/com/project1/smart_diary/repository/DiaryRepository.java
+++ app/backend/smart-diary/src/main/java/com/project1/smart_diary/repository/DiaryRepository.java
@@ -1,9 +1,18 @@
 package com.project1.smart_diary.repository;
 
 import com.project1.smart_diary.entity.DiaryEntity;
+import com.project1.smart_diary.entity.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
 @Repository
 public interface DiaryRepository extends JpaRepository<DiaryEntity,Long> {
+    List<DiaryEntity> findByUser_Email(String email);
+    List<DiaryEntity> findByUser_EmailAndCreatedAtBetween(String email, LocalDateTime fromDate, LocalDateTime toDate);
+    List<DiaryEntity> findByUser_EmailAndCreatedAtAfter(String email, LocalDateTime fromDate);
+    List<DiaryEntity> findByUser_EmailAndCreatedAtBefore(String email, LocalDateTime toDate);
+
 }
