cd1bfe0e46ce77036c060ab7737dd02eed6ebbc2
Quan Than Van <quantv654@gmail.com>
2025-10-04 21:59:56 +0700

    feat: thêm AuthGaurd để bảo vệ các private route

--- app/frontend/src/app/(app)/layout.tsx
+++ app/frontend/src/app/(app)/layout.tsx
@@ -1,6 +1,7 @@
 import BottomNavigation from '@/app/(app)/_components/BottomNavigation'
 import Sidebar from '@/app/(app)/_components/Sidebar'
 import Loading from '@/app/(app)/loading'
+import { AuthGuard } from '@/components/AuthGuard'
 import { Suspense } from 'react'
 
 export default function DashboardLayout({
@@ -9,18 +10,20 @@ export default function DashboardLayout({
 	children: React.ReactNode
 }>) {
 	return (
-		<div className="flex h-screen bg-diary-bg-light dark:bg-diary-bg-dark transition-colors">
-			<Sidebar />
+		<AuthGuard>
+			<div className="flex h-screen bg-diary-bg-light dark:bg-diary-bg-dark transition-colors">
+				<Sidebar />
 
-			<div className="flex-1 flex flex-col">
-				<Suspense fallback={<Loading />}>
-					<main className="flex-1 overflow-auto">{children}</main>
-				</Suspense>
-			</div>
+				<div className="flex-1 flex flex-col">
+					<Suspense fallback={<Loading />}>
+						<main className="flex-1 overflow-auto">{children}</main>
+					</Suspense>
+				</div>
 
-			<div className="lg:hidden">
-				<BottomNavigation />
+				<div className="lg:hidden">
+					<BottomNavigation />
+				</div>
 			</div>
-		</div>
+		</AuthGuard>
 	)
 }
