850c883e890cf65bfffe114ca035525964801c11
LeDanhTien <danhtien2k4@gmail.com>
2025-10-06 13:58:52 +0700

    fix: gộp chức năng tìm kiếm theo khoảng thời gian và cảm xúc

--- app/backend/smart-diary/src/main/java/com/project1/smart_diary/dto/request/DiarySearchRequest.java
+++ app/backend/smart-diary/src/main/java/com/project1/smart_diary/dto/request/DiarySearchRequest.java
@@ -1,16 +1,18 @@
 package com.project1.smart_diary.dto.request;
 
-
 import lombok.*;
 import org.springframework.format.annotation.DateTimeFormat;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
+
 @Getter
 @Setter
-public class DiarySearchByDateRequest {
+public class DiarySearchRequest {
     @DateTimeFormat(pattern = "yyyy-MM-dd")
     private LocalDate fromDate;
     @DateTimeFormat(pattern = "yyyy-MM-dd")
     private LocalDate toDate;
 
+    private String Emotion;
 }
--- app/backend/smart-diary/src/main/java/com/project1/smart_diary/repository/DiaryRepository.java
+++ app/backend/smart-diary/src/main/java/com/project1/smart_diary/repository/DiaryRepository.java
@@ -16,4 +16,7 @@ public interface DiaryRepository extends JpaRepository<DiaryEntity,Long> {
     List<DiaryEntity> findByUser_EmailAndCreatedAtAfter(String email, LocalDateTime fromDate);
     List<DiaryEntity> findByUser_EmailAndCreatedAtBefore(String email, LocalDateTime toDate);
     List<DiaryEntity> findByUser_EmailAndEmotion(String email, Emotion emotion);
+    List<DiaryEntity> findByUser_EmailAndEmotionAndCreatedAtBetween(String email, Emotion emotion, LocalDateTime fromDate, LocalDateTime toDate);
+    List<DiaryEntity> findByUser_EmailAndEmotionAndCreatedAtAfter(String email,Emotion emotion, LocalDateTime fromDate);
+    List<DiaryEntity> findByUser_EmailAndEmotionAndCreatedAtBefore(String email, Emotion emotion, LocalDateTime toDate);
 }
